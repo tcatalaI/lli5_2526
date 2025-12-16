df828521483e5b4cad6cc387b330568b5995a9ae
Bartomeu Català <tcatala@iedib.net>
2025-12-16 10:49:27 +0100

    LLevar opció paisos de paquet

--- Cromo.java
+++ Cromo.java
@@ -82,13 +82,5 @@ public static int getCromosNoMundial() {
                 '}';
     }
 
-    public static void main(String args[]) {
-        Cromo c1, c2, c3, c4, c5;
-        c1 = new Cromo ("esp", 2025, 1.76, 67,"P", "Pichford", LocalDate.of(2023, 12, 25));
-        c2 = new Cromo ("ITL", 2025, 1.76, 67,"D", "Mbappe", LocalDate.of(2023, 12, 25));
-        c3 = new Cromo ("USA", 2025, 1.76, 67,"M", "xxxx", LocalDate.of(2023, 12, 25));
-        c4 = new Cromo ("MEX", 2025, 1.76, 67,"M", "M", LocalDate.of(2023, 12, 25));
-        c5 = new Cromo ("CAN", 2025, 1.76, 67,"p", "M", LocalDate.of(2023, 12, 25));
-        System.out.println("No classificats " + Cromo.getCromosNoMundial());
-    }
+
 }
--- Paquet.java
+++ Paquet.java
@@ -1,17 +1,10 @@
-import java.time.LocalDate;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Paquet {
     private static final int CAPACITAT = 5;
    private  ArrayList<Cromo> cromos;
-    private final String paisos[] = { "USA","MEX","CAN","JPN", "IRN", "UZB",
-            "KOR", "JOR", "AUS","ARG", "BRA", "ECU","COL",  "URY",
-            "PAR", "MAR","TUN", "EGY","ALG",
-            "GHA", "CPV","ZAF", "SEN",
-            "AUT","BEL","CRO",  "ENG",
-            "FRA","GER","NED", "NOR",
-            "POR","ESP","SUI","SCO"};
 
     public Paquet() {
     // Constructor que genera un paquet amb 5 cromos aleatoris
@@ -33,9 +26,9 @@ public class Paquet {
     private String pais(short numero) {
      // Donat un nombre de cromo, ens retorna el pais corresponent
         byte index = (byte) (numero / 18);
-        if (index > paisos.length - 1)
+        if (index > Mundial.nombrePaisosClassificats() - 1)
             index = 0;
-        return paisos[index];
+        return Mundial.getPais(index);
     }
 
 
